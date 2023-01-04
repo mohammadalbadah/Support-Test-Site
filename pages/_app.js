cdce4d7f9a0d999cecf5ca0a35f9b43ca8da2e2f
hamza1995at <hamza1995at@gmail.com>
2023-01-04 17:32:18 +0200

    Adding logo

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,8 @@
 import '../styles/globals.css'
+import Home from '.'
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+function MyApp({ }) {
+  return <Home />
 }
 
 export default MyApp
--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,20 @@ import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import Script from 'next/script'
 
+function loadPage(store) {
+  switch (store) {
+    case 'arsenal':
+      open('https://www.arsenal.com/', '_self')
+      break;
+    case 'barcelona':
+      open('https://www.fcbarcelona.com/', '_self')
+      break;
+    case 'realmadrid':
+      open('https://www.realmadrid.com/', '_self')
+      break;
+    default: return;
+  }
+}
 
 export default function Home() {
   return (
@@ -15,8 +29,13 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to Contentsquare!
+          Choose your club
         </h1>
+        <li className={styles.grid}>
+          <img src={'https://logos-world.net/wp-content/uploads/2020/05/Arsenal-Logo.png'} onClick={() => { loadPage('arsenal') }} height={200} />
+          <img src={'https://logos-world.net/wp-content/uploads/2020/04/Barcelona-Logo.png'} onClick={() => { loadPage('barcelona') }} height={200} />
+          <img src={'https://logos-world.net/wp-content/uploads/2020/06/Real-Madrid-symbol.png'} onClick={() => { loadPage('realmadrid') }} height={200} />
+        </li>
 
       </main>
 
@@ -24,6 +43,36 @@ export default function Home() {
 
       </footer>
       <Script type="text/javascript" src="https://t.contentsquare.net/uxa/2c5142b15f133.js"></Script>
+    </div>
+  )
+}
+
+export function Arsenal() {
+  return (
+    <div>
+      <h1>
+        Arsenal Store
+      </h1>
+    </div>
+  )
+}
+
+export function Barclona() {
+  return (
+    <div>
+      <h1>
+        Barclona Store
+      </h1>
+    </div>
+  )
+}
+
+export function RealMadrid() {
+  return (
+    <div>
+      <h1>
+        RealMadrid Store
+      </h1>
     </div>
   )
 }
