b6f3ff9ef7203d11b2f9d8f265992ceec1345092
hamza1995at <hamza1995at@gmail.com>
2023-01-09 16:18:27 +0200

    added keys

--- components/ProductList.js
+++ components/ProductList.js
@@ -3,22 +3,26 @@ import styles from '../styles/Home.module.css'
 
 
 const realProducts = [{
+    id: 1,
     club: 'Real Madrid',
     imageSrc: 'https://cdn.shopify.com/s/files/1/0448/3609/4115/products/HF0292-RMCFMZ0075-02_500x480.jpg?v=1655974535',
     productName: "Mens Home Authentic Shirt 22/23",
     productPrice: '140.00'
 },
 {
+    id: 2,
     club: 'Real Madrid',
     imageSrc: 'https://cdn.shopify.com/s/files/1/0448/3609/4115/products/RMCFMS0173-01_500x480.jpg?v=1671195336',
     productName: 'Tracksuit Jacket adidas Men Training',
     productPrice: '85.00'
 }, {
+    id: 3,
     club: 'Real Madrid',
     imageSrc: 'https://cdn.shopify.com/s/files/1/0448/3609/4115/products/RMCFMP0128-01_500x480.jpg?v=1671195262',
     productName: "Bottoms Technical PRO adidas Men's Training Purple",
     productPrice: '85.00'
 }, {
+    id: 4,
     club: 'Real Madrid',
     imageSrc: 'https://cdn.shopify.com/s/files/1/0448/3609/4115/products/RMCFMS0171-01_500x480.jpg?v=1671458610',
     productName: 'Warm-up sweatshirt adidas Man',
@@ -31,7 +35,7 @@ const ProductList = ({ }) => {
             <div className={styles.grid}>
                 {
                     realProducts.map((product) => (
-                        <ProductCard cardDetails={product} />
+                        <ProductCard key={product.id} cardDetails={product} />
                     ))
                 }
             </div>
