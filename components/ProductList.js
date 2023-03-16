502c588f615c9295db743ac7f6c92fe33d42fc82
hamza1995at <hamza1995at@gmail.com>
2023-03-16 14:10:58 +0200

    Update ProductList.js

--- components/ProductList.js
+++ components/ProductList.js
@@ -1,5 +1,6 @@
 import ProductCard from "./ProductCard";
 import styles from '../styles/Home.module.css'
+import { useRouter } from 'next/router';
 
 
 const realProducts = [{
@@ -29,18 +30,70 @@ const realProducts = [{
     productPrice: '110.00'
 }]
 
+const arsenalProducts = [{
+    id: 1,
+    club: 'Real Madrid',
+    imageSrc: 'https://cdn.shopify.com/s/files/1/0448/3609/4115/products/HF0292-RMCFMZ0075-02_500x480.jpg?v=1655974535',
+    productName: "Mens Home Authentic Shirt 22/23",
+    productPrice: '140.00'
+}]
+
+const barcelonaProducts = [{
+    id: 3,
+    club: 'Real Madrid',
+    imageSrc: 'https://cdn.shopify.com/s/files/1/0448/3609/4115/products/RMCFMP0128-01_500x480.jpg?v=1671195262',
+    productName: "Bottoms Technical PRO adidas Men's Training Purple",
+    productPrice: '85.00'
+}, {
+    id: 4,
+    club: 'Real Madrid',
+    imageSrc: 'https://cdn.shopify.com/s/files/1/0448/3609/4115/products/RMCFMS0171-01_500x480.jpg?v=1671458610',
+    productName: 'Warm-up sweatshirt adidas Man',
+    productPrice: '110.00'
+}]
+
 const ProductList = ({ }) => {
-    return (
-        <>
-            <div className={styles.grid}>
-                {
-                    realProducts.map((product) => (
-                        <ProductCard key={product.id} cardDetails={product} />
-                    ))
-                }
-            </div>
-        </>
-    );
+    const router = useRouter();
+    var query = router.asPath.split('?')[1];
+    if (query == 'arsenal') {
+        return (
+            <>
+                <div className={styles.grid}>
+                    {
+                        arsenalProducts.map((product) => (
+                            <ProductCard key={product.id} cardDetails={product} />
+                        ))
+                    }
+                </div>
+            </>
+        );
+    }
+    else if (query == 'barcelona') {
+        return (
+            <>
+                <div className={styles.grid}>
+                    {
+                        barcelonaProducts.map((product) => (
+                            <ProductCard key={product.id} cardDetails={product} />
+                        ))
+                    }
+                </div>
+            </>
+        );
+    } else if (query == 'realmadrid') {
+        return (
+            <>
+                <div className={styles.grid}>
+                    {
+                        realProducts.map((product) => (
+                            <ProductCard key={product.id} cardDetails={product} />
+                        ))
+                    }
+                </div>
+            </>
+        );
+    }
+    else return;
 }
 
 export default ProductList;
