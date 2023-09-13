408b7291d70d852863e03eb4979c18678afb65cc
Mo Al-Badah <mohammadalbadah@gmail.com>
2023-09-13 11:34:58 -0500

    Revert "Support Tag Management Switching"
    This reverts commit bfdd53c063ce9e6304c9b697003dac9c981fa1dd.

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,40 +1,10 @@
 import Link from 'next/link';
-import { useState } from "react"
-
 
 const Navbar = () => {
-let tagDeployValue;
-
-  try {
-    tagDeployValue = localStorage.getItem("tagDeployMethod") || "0";
-    localStorage.setItem("tagDeployMethod", tagDeployValue);
-  } catch (error) {}
-
-  const [tagDeployMethod] = useState(tagDeployValue);
-
-  const saveToLocalStorage = e => {
-    e.preventDefault()
-    var value;
-    let deployText;
-
-    switch (tagDeployMethod){
-      case "0": value = "1"
-      deployText = "Adobe Launch"
-      break;
-      case "1": value = "0"
-      deployText = "GTM"
-      break;
-      default: value = "0"
-      break;
-    }
-    localStorage.setItem("tagDeployMethod", value);
-    alert(`Switching Tag Deployment Mode to ${deployText}`)
-    window.location.reload();
-  }
     return (
         <nav>
             <div className="logo">
-                <a onClick={saveToLocalStorage}><h1>Support Site</h1></a>
+                <h1>Support Site</h1>
             </div>
             <Link href="/">Home</Link>
             <Link href={{ pathname: "/products", query: "arsenal" }}>Arsenal</Link>
--- pages/_app.js
+++ pages/_app.js
@@ -7,12 +7,6 @@ import Script from 'next/script';
 
 
 function MyApp({ Component, pageProps }) {
-  
-let tagDeployValue;
-
-try {
-  tagDeployValue = localStorage.getItem("tagDeployMethod") || "0";
-} catch (error) {}
 
   const router = useRouter();
   var path = "";
@@ -30,14 +24,13 @@ try {
 
   return (
     <Layout>
-      {tagDeployValue == "0" && (<Script id="GTM" strategy="afterInteractive" dangerouslySetInnerHTML={{
+      <Script id="GTM" strategy="afterInteractive" dangerouslySetInnerHTML={{
         __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
         new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
         j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
         'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
         })(window,document,'script','dataLayer','GTM-W989V5M');`}}>
-      </Script>) || tagDeployValue == "1" && (<Script id="AdobeLaunch" strategy="afterInteractive" src="https://assets.adobedtm.com/ae93c31080b0/30f55a047a18/launch-b6b198979163-development.min.js">
-      </Script>)}
+      </Script>
       <Head>
         <title>{path}</title>
         <link rel="icon" href="https://assets.contentsquare.com/design-tokens/assets/brand/logo/favicon.png" sizes="any" />
