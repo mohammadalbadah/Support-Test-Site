c549ddcc1476d94b259ebdbad2390f8efffc0f70
Mo Al-Badah <mohammadalbadah@gmail.com>
2023-09-14 15:36:02 -0500

    Support Tag Management Switching
    -Clicking on the Support Site text on the navbar will switch the tag deployment script from GTM to Adobe and vice versa

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,10 +1,40 @@
 import Link from 'next/link';
+import { useState } from "react"
+
 
 const Navbar = () => {
+let tagDeployValue;
+
+  try {
+    tagDeployValue = localStorage.getItem("tagDeployMethod") || "0";
+    localStorage.setItem("tagDeployMethod", tagDeployValue);
+  } catch (error) {}
+
+  const [tagDeployMethod] = useState(tagDeployValue);
+
+  const saveToLocalStorage = e => {
+    e.preventDefault()
+    var value;
+    let deployText;
+
+    switch (tagDeployMethod){
+      case "0": value = "1"
+      deployText = "Adobe Launch"
+      break;
+      case "1": value = "0"
+      deployText = "GTM"
+      break;
+      default: value = "0"
+      break;
+    }
+    localStorage.setItem("tagDeployMethod", value);
+    alert(`Switching Tag Deployment Mode to ${deployText}`)
+    window.location.reload();
+  }
     return (
         <nav>
             <div className="logo">
-                <h1>Support Site</h1>
+                <a onClick={saveToLocalStorage}><h1>Support Site</h1></a>
             </div>
             <Link href="/">Home</Link>
             <Link href={{ pathname: "/products", query: "arsenal" }}>Arsenal</Link>
